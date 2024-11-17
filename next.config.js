058fd2cb1da5b8ce7b91b4731a310dc29456cda8
Malki Abderrahmane <malki.abdurrahmane@gmail.com>
2024-11-17 20:13:24 +0100

    fix: update dependencies and next configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@shikijs/twoslash'],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
